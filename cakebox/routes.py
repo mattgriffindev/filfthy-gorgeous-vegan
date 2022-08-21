f91f8600295e5da0e9df6fd0dd17d28fe8ffff13
Matt <matt.griffin@live.co.uk>
2022-08-21 23:53:20 +0000

    Add cakebox/templates/index.html; Add hero, quick-links & features sections to index.html; Add hero, quick-links & features styling in style.css; Amend index() to render template index.html in routes.py

--- cakebox/routes.py
+++ cakebox/routes.py
@@ -2,6 +2,7 @@ from flask import render_template, request, redirect, url_for
 from cakebox import app, db
 from cakebox.models import Category, Recipe
 
+
 @app.route("/")
-def home():
-    return render_template("base.html")
+def index():
+    return render_template("index.html")
