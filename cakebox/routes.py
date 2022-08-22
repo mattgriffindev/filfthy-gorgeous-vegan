84608a93888f4b5baef9ff8d48499b77615ceb4d
Matt <matt.griffin@live.co.uk>
2022-08-22 00:13:12 +0000

    Add routes to render templates for {about.html, add_category.html, add_recipe.html, categories.html, contact.html, edit_category.html, edit_recipe.html, login.html, privacy.html, recipes.html, register.html, terms.html}

--- cakebox/routes.py
+++ cakebox/routes.py
@@ -6,3 +6,53 @@ from cakebox.models import Category, Recipe
 @app.route("/")
 def index():
     return render_template("index.html")
+
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+
+@app.route("/add_category")
+def add_category():
+    return render_template("add_category.html")
+    
+
+@app.route("/add_recipe")
+def add_recipe():
+    return render_template("add_recipe.html")
+
+
+@app.route("/contact")
+def contact():
+    return render_template("contact.html")
+    
+
+@app.route("/edit_category")
+def edit_category():
+    return render_template("edit_category.html")
+
+
+@app.route("/edit_recipe")
+def edit_recipe():
+    return render_template("edit_recipe.html")
+
+
+@app.route("/login")
+def login():
+    return render_template("login.html")
+    
+
+@app.route("/privacy")
+def privacy():
+    return render_template("privacy.html")
+
+
+@app.route("/register")
+def register():
+    return render_template("register.html")
+
+
+@app.route("/terms")
+def terms():
+    return render_template("terms.html")
