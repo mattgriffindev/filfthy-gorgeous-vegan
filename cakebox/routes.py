f5a706eb8f3d436fcb2dc6961c60a7689cb124c1
Matt <matt.griffin@live.co.uk>
2022-08-25 00:11:04 +0000

    Amend routes.py to add routes for categories, add_category, edit_category & delete_category; Add content to categories.html, add_category.html & edit_category to create, read and update data

--- cakebox/routes.py
+++ cakebox/routes.py
@@ -13,10 +13,39 @@ def about():
     return render_template("about.html")
 
 
-@app.route("/add_category")
+@app.route("/categories")
+def categories():
+    categories = list(Category.query.order_by(Category.category_name).all())
+    return render_template("categories.html", categories=categories)
+
+
+@app.route("/add_category", methods=["GET", "POST"])
 def add_category():
+    if request.method == "POST":
+        category = Category(category_name=request.form.get("category_name"))
+        db.session.add(category)
+        db.session.commit()
+        return redirect(url_for("categories"))
     return render_template("add_category.html")
-    
+
+
+@app.route("/edit_category/<int:category_id>", methods=["GET", "POST"])
+def edit_category(category_id):
+    category = Category.query.get_or_404(category_id)
+    if request.method == "POST":
+        category.category_name = request.form.get("category_name")
+        db.session.commit()
+        return redirect(url_for("categories"))
+    return render_template("edit_category.html", category=category)
+
+
+@app.route("/delete_category/<int:category_id>")
+def delete_category(category_id):
+    category = Category.query.get_or_404(category_id)
+    db.session.delete(category)
+    db.session.commit()
+    return redirect(url_for("categories"))
+
 
 @app.route("/add_recipe")
 def add_recipe():
@@ -27,11 +56,6 @@ def add_recipe():
 def contact():
     return render_template("contact.html")
     
-
-@app.route("/edit_category")
-def edit_category():
-    return render_template("edit_category.html")
-
 
 @app.route("/edit_recipe")
 def edit_recipe():
