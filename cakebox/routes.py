699fd19276f321d50e45966b0f8f2ce4bb8da5ca
Matt <matt.griffin@live.co.uk>
2022-08-27 07:36:31 +0000

    Amend recipes.html & add_recipe.html - add content

--- cakebox/routes.py
+++ cakebox/routes.py
@@ -47,20 +47,10 @@ def delete_category(category_id):
     return redirect(url_for("categories"))
 
 
-@app.route("/add_recipe")
-def add_recipe():
-    return render_template("add_recipe.html")
-
-
 @app.route("/contact")
 def contact():
     return render_template("contact.html")
     
-
-@app.route("/edit_recipe")
-def edit_recipe():
-    return render_template("edit_recipe.html")
-
 
 @app.route("/login")
 def login():
@@ -79,7 +69,51 @@ def register():
 
 @app.route("/recipes")
 def recipes():
-    return render_template("recipes.html")
+    recipes = list(Recipe.query.order_by(Recipe.id).all())
+    return render_template("recipes.html", recipes=recipes)
+
+
+@app.route("/add_recipe", methods=["GET", "POST"])
+def add_recipe():
+    categories = list(Category.query.order_by(Category.category_name).all())
+    if request.method == "POST":
+        recipe = Recipe(
+            recipe_name=request.form.get("recipe_name"),
+            recipe_desc=request.form.get("recipe_desc"),
+            recipe_prep=request.form.get("recipe_prep"),
+            recipe_bake=request.form.get("recipe_bake"),
+            recipe_serves=request.form.get("recipe_serves"),
+            recipe_difficulty=request.form.get("recipe_difficulty"),
+            category_id=request.form.get("category_id")
+        )
+        db.session.add(recipe)
+        db.session.commit()
+        return redirect(url_for("recipes"))
+    return render_template("add_recipe.html", categories=categories)
+
+
+@app.route("/edit_recipe/<int:recipe_id>", methods=["GET", "POST"])
+def edit_recipe(recipe_id):
+    recipe = Recipe.query.get_or_404(recipe_id)
+    categories = list(Category.query.order_by(Category.category_name).all())
+    if request.method == "POST":
+        recipe.recipe_name=request.form.get("recipe_name"),
+        recipe.recipe_desc=request.form.get("recipe_desc"),
+        recipe.recipe_prep=request.form.get("recipe_prep"),
+        recipe.recipe_bake=request.form.get("recipe_bake"),
+        recipe.recipe_serves=request.form.get("recipe_serves"),
+        recipe.recipe_difficulty=request.form.get("recipe_difficulty"),
+        recipe.category_id=request.form.get("category_id")
+        db.session.commit()
+    return render_template("edit_recipe.html", recipe=recipe, categories=categories)
+
+
+@app.route("/delete_recipe/<int:recipe_id>")
+def delete_recipe(recipe_id):
+    recipe = Recipe.query.get_or_404(recipe_id)
+    db.session.delete(recipe)
+    db.session.commit()
+    return redirect(url_for("recipes"))
 
 
 @app.route("/terms")
