8a495496a5b7918ac6977277a9611b2e717e93c2
Matt <matt.griffin@live.co.uk>
2022-09-17 13:34:30 +0000

    Edit models.py: remove recipe col from category table

--- cakebox/models.py
+++ cakebox/models.py
@@ -16,7 +16,6 @@ class Category(db.Model):
     # schema for the Category model
     id = db.Column(db.Integer, primary_key=True)
     category_name = db.Column(db.String(25), unique=True, nullable=False)
-    recipes = db.relationship("Recipe", backref="category", cascade="all, delete", lazy=True)
 
     def __repr__(self):
         # __repr__ to represent itself in the form of a string
